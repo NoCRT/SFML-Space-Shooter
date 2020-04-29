a63b6d7f90261a30f416a8456ff9833455a2a852
NoCRT <39925234+NoCRT@users.noreply.github.com>
2020-04-29 05:15:08 +0100

    clean repo

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Aidan/Documents/GitHub/SFML-Space-Shooter
-# Build directory: C:/Users/Aidan/Documents/GitHub/SFML-Space-Shooter/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
